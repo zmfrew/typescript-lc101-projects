90d46c08a36d0f6793c63cfb2d8b9b15f9cd3403
Zach Frew <zachary.frew@ascension-external.org>
2020-03-23 20:37:52 -0500

    Add finished studio

---
+++ studio/Rocket.js
@@ -0,0 +1,39 @@
+"use strict";
+exports.__esModule = true;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        return items.reduce(function (sum, item) { return sum + item.massKg; }, 0);
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
---
+++ studio/Rocket.ts
@@ -0,0 +1,45 @@
+import { Astronaut } from './Astronaut'
+import { Cargo } from './Cargo'
+import { Payload } from './Payload'
+
+export class Rocket {
+    name: string
+    totalCapacityKg: number
+    cargoItems: Cargo[] = []
+    astronauts: Astronaut[] = []
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name
+        this.totalCapacityKg = totalCapacityKg
+    }
+
+    sumMass(items: Payload[]): number {
+        return items.reduce((sum, item) => sum + item.massKg, 0)
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems)
+    }
+
+    canAdd(item: Payload): boolean {
+        return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo)
+            return true
+        } else {
+            return false
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut)
+            return true
+        } else {
+            return false
+        }
+    }
+}
